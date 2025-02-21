ebd3deb224f4f97ede50d671929bbd48f34079fa
DingJunyao <dingjunyao0703@163.com>
2025-02-21 23:16:25 +0800

    gpx2csv add area recognize

--- gpx2csv.py
+++ gpx2csv.py
@@ -1,8 +1,13 @@
 import csv
 import math
 from typing import List
+import os
+import sqlite3
 
 import gpxpy.gpx
+import geopandas as gpd
+from geopandas import GeoDataFrame
+from shapely.geometry import Point
 
 # 计算方位角
 def calculate_bearing(point1, point2):
@@ -18,7 +23,40 @@ def calculate_bearing(point1, point2):
     bearing = (bearing + 360) % 360
     return bearing
 
-def segment_to_dict_list(segment: gpxpy.gpx.GPXTrackSegment) -> List[dict]:
+def load_area_gdf_list(geojson_dir: str) -> List[GeoDataFrame]:
+    gdf_list = []
+    # 读取目录里所有文件
+    for filename in os.listdir(geojson_dir):
+        if filename.endswith(".json") or filename.endswith(".geojson"):
+            gdf_list.append(gpd.read_file(os.path.join(geojson_dir, filename)))
+    return gdf_list
+
+def get_area_id(point: Point, area_gdf_list: List[GeoDataFrame]) -> str:
+    for i, gdf in enumerate(area_gdf_list):
+        for index, row in gdf.iterrows():
+            polygon = row['geometry']  # 获取多边形
+            if polygon.contains(point):  # 判断点是否在多边形内
+                # print(f"点 ({point.x}, {point.y}) 在区域 {row['id']} 中，区域名称为 {row['name']}")
+                return row['id']
+    raise ValueError(f"点 ({point.x}, {point.y}) 不在任何已知区域内")
+
+def get_area_info(point: Point, area_gdf_list: List[GeoDataFrame], area_code_conn: sqlite3.Connection):
+    cursor = area_code_conn.cursor()
+    area_id = get_area_id(point, area_gdf_list)
+    sql = """
+    select province.name, city.name, area.name
+    from province, city, area
+    where
+        province.code = area.provinceCode
+        and city.code = area.cityCode
+        and area.code = ?
+    """
+    cursor.execute(sql, (area_id,))
+    result = cursor.fetchone()
+    cursor.close()
+    return result
+
+def segment_to_dict_list(segment: gpxpy.gpx.GPXTrackSegment, area_gdf_list: List[GeoDataFrame], area_code_conn: sqlite3.Connection) -> List[dict]:
     first_point = segment.points[0]
     course = 0
     total_distance = 0
@@ -43,6 +81,7 @@ def segment_to_dict_list(segment: gpxpy.gpx.GPXTrackSegment) -> List[dict]:
             course = 0
         # print(index, point.time_difference(first_point), point.longitude, point.latitude, point.elevation,
         #       total_distance, course, speed * 3600 / 1000)
+        area_info = get_area_info(Point(point.longitude, point.latitude), area_gdf_list, area_code_conn)
         ret_list.append({
             'index': index,
             'time': point.time,
@@ -52,17 +91,20 @@ def segment_to_dict_list(segment: gpxpy.gpx.GPXTrackSegment) -> List[dict]:
             'elevation': point.elevation,
             'distance': total_distance,
             'course': course,
-            'speed': speed
+            'speed': speed,
+            'province': area_info[0],
+            'city': area_info[1],
+            'area': area_info[2],
         })
     return ret_list
 
-def single_segment_gpx_to_dict_list(gpx: gpxpy.gpx.GPX) -> List[dict]:
-    return segment_to_dict_list(gpx.tracks[0].segments[0])
+def single_segment_gpx_to_dict_list(gpx: gpxpy.gpx.GPX, area_gdf_list: List[GeoDataFrame], area_code_conn: sqlite3.Connection) -> List[dict]:
+    return segment_to_dict_list(gpx.tracks[0].segments[0], area_gdf_list, area_code_conn)
 
-def single_segment_gpx_file_path_to_dict_list(gpx_file_path: str) -> List[dict]:
+def single_segment_gpx_file_path_to_dict_list(gpx_file_path: str, area_gdf_list: List[GeoDataFrame], area_code_conn: sqlite3.Connection) -> List[dict]:
     with open(gpx_file_path, 'r') as gpx_file:
         gpx = gpxpy.parse(gpx_file)
-        return single_segment_gpx_to_dict_list(gpx)
+        return single_segment_gpx_to_dict_list(gpx, area_gdf_list, area_code_conn)
 
 def dict_list_to_csv(dict_list: List[dict], csv_file_path: str):
     fieldnames = dict_list[0].keys()
@@ -80,9 +122,11 @@ def dict_list_to_csv(dict_list: List[dict], csv_file_path: str):
             write_row['time'] = write_row['time'].strftime('%Y-%m-%d %H:%M:%S')
             writer.writerow(write_row)
 
-def single_segment_gpx_file_path_to_csv(gpx_file_path: str, csv_file_path: str):
-    dict_list = single_segment_gpx_file_path_to_dict_list(gpx_file_path)
+def single_segment_gpx_file_path_to_csv(gpx_file_path: str, csv_file_path: str, area_gdf_list: List[GeoDataFrame], area_code_conn: sqlite3.Connection):
+    dict_list = single_segment_gpx_file_path_to_dict_list(gpx_file_path, area_gdf_list, area_code_conn)
     dict_list_to_csv(dict_list, csv_file_path)
 
 if __name__ == '__main__':
-    single_segment_gpx_file_path_to_csv('test/test.gpx', 'test/test_self.csv')
+    gdf_list = load_area_gdf_list(r"asset\area_geojson")
+    conn = sqlite3.connect(r"asset\area_code.sqlite")
+    single_segment_gpx_file_path_to_csv('test/test.gpx', 'test/test.csv', gdf_list, conn)
